732c61a2a1a97b06656c9a259e041cc49f7a3808
Dino Lozina <Dino.Lozina@macrometa.com>
2023-03-27 11:56:48 +0200

    Updated jsc8 SDK version

---
+++ read/get-graph.js
@@ -0,0 +1,19 @@
+const jsc8 = require("jsc8");
+const dotenv = require("dotenv");
+const path = require("path");
+
+const envPath = path.join(__dirname, "..", ".env");
+dotenv.config({ path: envPath });
+
+client = new jsc8({
+  url: process.env.BASE_URL,
+  apiKey: process.env.API_KEY,
+  fabricName: process.env.FABRIC,
+});
+
+// Please be sure that edge collection and edge have been created before running this script.
+async function getEdgeFromEdgeCollection() {
+  const response = await client.getGraph("grocery-graph");
+  console.log(response);
+}
+getEdgeFromEdgeCollection();
